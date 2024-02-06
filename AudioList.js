5c3a951f7fa2d95700d55f1b9b9030c438f57d1b
McVITIES755 <mcvities857@gmail.com>
2024-02-06 20:20:53 +0000

    commit
    commit

---
+++ AudioList.js
@@ -0,0 +1,158 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the Images folder of this project folder",
+//     src: "music name here - remember img must be in  formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+    {
+      name: "Starboy",
+      artist: "Weeknd and Daft Punk",
+      img: "Images/Starboy.jpg",
+      src: "Starboy"
+    },
+    {
+      name: "Despacito",
+      artist: "Luis Fonsi and Daddy Yankee ft. Justin Beiber",
+      img: "Images/Despacito.jpg",
+      src: "Despacito"
+    },
+    {
+      name: "Nonsense",
+      artist: "Sabrina Carpenter",
+      img: "Images/Nonsense.jpg",
+      src: "Nonsense"
+    },
+    {
+      name: "Save Your Tears (Remix)",
+      artist: "The Weeknd and Ariana Grande",
+      img: "Images/Save-Your-Tears.jpg",
+      src: "Save-Your-Tears"
+    },
+    {
+      name: "Cheap Thrills",
+      artist: "Sia and Sean Paul",
+      img: "Images/Cheap-Thrills.jpg",
+      src: "Cheap-Thrills"
+    },
+    {
+      name: "Feather",
+      artist: "Sabrina Carpenter",
+      img: "Images/Feather.jpg",
+      src: "Feather"
+    },
+    {
+      name: "Derniere Danse",
+      artist: "Indila",
+      img: "Images/Derniere-Danse.jpg",
+      src: "Derniere-Danse"
+    },
+    {
+      name: "Tourner Dans Le Vide",
+      artist: "Indila",
+      img: "Images/Tourner-Dans-Le-Vide.jpg",
+      src: "Tourner-Dans-Le-Vide"
+    },
+    {
+      name: "Clean Up",
+      artist: "Chunkz and Yung Filly",
+      img: "Images/Clean-Up.jpg",
+      src: "Clean-Up"
+    },
+    {
+      name: "Feel This Moment",
+      artist: "Pitbull and Christina Aguilera",
+      img: "Images/Feel-This-Moment.jpg",
+      src: "Feel-This-Moment"
+    },
+    {
+      name: "We Own It",
+      artist: "Wiz Khalifa and 2 Chainz",
+      img: "Images/We-Own-It.jpg",
+      src: "We-Own-It"
+    },
+    {
+      name: "Ride Out",
+      artist: "Kid Ink, Tyga, YG and Rich Homie Quan",
+      img: "Images/Ride-Out.jpg",
+      src: "Ride-Out"
+    },
+    {
+      name: "Go Hard or Go Home",
+      artist: "Wiz Khalifa and Iggy Azalea",
+      img: "Images/Go-Hard-Or-Go-Home.jpg",
+      src: "Go-Hard-Or-Go-Home"
+    },
+    {
+      name: "Kick Down The Door",
+      artist: "Harris J and AJ Jung",
+      img: "Images/Kick-Down-The-Door.jpg",
+      src: "Kick-Down-The-Door"
+    },
+    {
+      name: "One Dance",
+      artist: "Drake",
+      img: "Images/One-Dance.jpg",
+      src: "One-Dance"
+    },
+    {
+      name: "Taki Taki",
+      artist: "DJ Snake Selena Gomez Ozuna & Cardi B",
+      img: "Images/TakiTaki.jpg",
+      src: "TakiTaki"
+    },
+    {
+      name: "Unravel",
+      artist: "TK from 凛として時雨",
+      img: "Images/Unravel.jpg",
+      src: "Unravel"
+    },
+    {
+      name: "Middle Of The Night",
+      artist: "Elley Duhe",
+      img: "Images/Middle-Of-The-Night.jpg",
+      src: "Middle-Of-The-Night"
+    },
+    {
+      name: "Chandelier",
+      artist: "Sia",
+      img: "Images/Chandelier.jpg",
+      src: "Chandelier"
+    },
+    {
+        name: "Unstoppable",
+        artist: "Sia",
+        img: "Images/Unstoppable.jpg",
+        src: "Unstoppable"
+    },
+    {
+      name: "The Greatest",
+      artist: "Sia",
+      img: "Images/The-Greatest.jpg",
+      src: "The-Greatest"
+  },
+    {
+        name: "Dusk Till Dawn",
+        artist: "Zayn Malik and Sia",
+        img: "Images/Dust-Till-Dawn.jpg",
+        src: "Dusk-Till-Dawn"
+    },
+    {
+        name: "Another Love",
+        artist: "Tom Odell",
+        img: "Images/Another-Love.jpg",
+        src: "Another-Love"
+    },
+    // like this paste it and remember to give comma after ending of this bracket }
+    // {
+    //   name: "Here is the music name",
+    //   artist: "Here is the artist name",
+    //   img: "image name here - remember img must be in .jpg formate and it's inside the Images folder of this project folder",
+    //   src: "music name here - remember img must be in  formate and it's inside the songs folder of this project folder"
+    // }
+  ];
