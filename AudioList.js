cf08674e8b42a42d7a2d20ed42d3db8ecbc2dd18
McVITIES755 <mcvities857@gmail.com>
2024-02-07 20:18:37 +0000

    commit
    commit

--- AudioList.js
+++ AudioList.js
@@ -101,6 +101,12 @@ let allMusic = [
       src: "One-Dance"
     },
     {
+      name: "Carol Of The Bells",
+      artist: "Lindsay Stirling",
+      img: "Images/Carol-Of-The-Bells.jpg",
+      src: "Carol-Of-The-Bells"
+    },
+    {
       name: "Taki Taki",
       artist: "DJ Snake Selena Gomez Ozuna & Cardi B",
       img: "Images/TakiTaki.jpg",
